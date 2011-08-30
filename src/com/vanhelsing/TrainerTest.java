758bad151119a8b990e77b1165a1413ed0c96aed
Aneesh P U <aneeshpu@gmail.com>
2011-08-31 00:42:54 +0530

    implemented calculation of probability of document

--- src/com/vanhelsing/TrainerTest.java
+++ src/com/vanhelsing/TrainerTest.java
@@ -25,18 +25,30 @@ public class TrainerTest {
 	@Test
 	public void calculates_conditional_probability_of_a_given_feature() {
 
-		assertThat(trainer.conditionalProbability("money", Classification.BAD), IsEqual.<Float>equalTo(0.5f));
-		assertThat(trainer.conditionalProbability("pharmaceuticals", Classification.BAD), IsEqual.<Float>equalTo(0.5f));
-		assertThat(trainer.conditionalProbability("casino", Classification.BAD), IsEqual.<Float>equalTo(0.5f));
-		assertThat(trainer.conditionalProbability("quick", Classification.BAD), IsEqual.<Float>equalTo(0.5f));
-		assertThat(trainer.conditionalProbability("quick", Classification.GOOD), IsEqual.<Float>equalTo(2/3f));
-		assertThat(trainer.conditionalProbability("jumps", Classification.GOOD), IsEqual.<Float>equalTo(2/3f));
-		assertThat(trainer.conditionalProbability("water", Classification.GOOD), IsEqual.<Float>equalTo(1/3f));
-		assertThat(trainer.conditionalProbability("nobody", Classification.GOOD), IsEqual.<Float>equalTo(1/3f));
-		assertThat(trainer.conditionalProbability("fox", Classification.GOOD), IsEqual.<Float>equalTo(1/3f));
-		assertThat(trainer.conditionalProbability("nonexistentword", Classification.GOOD), IsEqual.<Float>equalTo(0f));
-		assertThat(trainer.conditionalProbability("nonexistentword", Classification.BAD), IsEqual.<Float>equalTo(0f));
+		assertThat(trainer.conditionalProbability("money", Classification.BAD), IsEqual.<Float> equalTo(0.5f));
+		assertThat(trainer.conditionalProbability("pharmaceuticals", Classification.BAD), IsEqual.<Float> equalTo(0.5f));
+		assertThat(trainer.conditionalProbability("casino", Classification.BAD), IsEqual.<Float> equalTo(0.5f));
+		assertThat(trainer.conditionalProbability("quick", Classification.BAD), IsEqual.<Float> equalTo(0.5f));
+		assertThat(trainer.conditionalProbability("quick", Classification.GOOD), IsEqual.<Float> equalTo(2 / 3f));
+		assertThat(trainer.conditionalProbability("jumps", Classification.GOOD), IsEqual.<Float> equalTo(2 / 3f));
+		assertThat(trainer.conditionalProbability("water", Classification.GOOD), IsEqual.<Float> equalTo(1 / 3f));
+		assertThat(trainer.conditionalProbability("nobody", Classification.GOOD), IsEqual.<Float> equalTo(1 / 3f));
+		assertThat(trainer.conditionalProbability("fox", Classification.GOOD), IsEqual.<Float> equalTo(1 / 3f));
+		assertThat(trainer.conditionalProbability("nonexistentword", Classification.GOOD), IsEqual.<Float> equalTo(0f));
+		assertThat(trainer.conditionalProbability("nonexistentword", Classification.BAD), IsEqual.<Float> equalTo(0f));
+	}
+
+	@Test
+	public void determines_the_conditional_probability_of_a_given_document() {
+
+		Document document = new Document("make quick money at the online casino");
+
+		assertThat(trainer.conditionalProbability(document, Classification.BAD), IsEqual.<Float> equalTo(0.0078f));
 	}
 	
-	
+	public void roundingtest() {
+		
+		float round = (float)Math.round(.078125 * 1000)/1000;
+		System.out.println(round);
+	}
 }
